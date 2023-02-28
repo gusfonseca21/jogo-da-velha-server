b3f353513104a004f0457567c7d4daf962182ac4
Gustavo Fonseca <gustavo.fonseca@cleversystems.com.br>
2023-02-28 18:20:30 -0300

    cors atualizado

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://rolezeiros-jogo-da-velha.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
